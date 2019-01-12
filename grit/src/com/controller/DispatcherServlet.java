9c78080c85e08ff0f4735a06776f6517075ce9e7
user <user@DESKTOP-GL9ESL1>
2019-01-12 20:21:33 +0900

    서블릿주석삭제

--- grit/src/com/controller/DispatcherServlet.java
+++ grit/src/com/controller/DispatcherServlet.java
@@ -19,29 +19,27 @@ public class DispatcherServlet extends HttpServlet {
     
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		//super.init(config);
-		//mapping ����(CommandPro.properties)�� �о properties �÷��ǿ� ����
 		
-		//1. �������� �о����
+		
 		String configFile=config.getInitParameter("configFile");
-		//=>CommandPro.properties
+
 		System.out.println("configFile= "+configFile);
 		
-		//2. �������� ���� ��� ���ϱ�
+	
 		String realConfigFile=config.getServletContext().getRealPath(configFile);
 		System.out.println("realConfigFile= "+realConfigFile);
 		
-		//�׽�Ʈ ���
+
 		realConfigFile="C:\\Users\\user\\Desktop\\gritlocal\\gritproject\\grit\\WebContent\\grit\\config\\CommandPro.properties";
 		System.out.println("�׽�Ʈ ���= "+realConfigFile+"\n\n");
 		
-		//3. Properties �÷��ǿ� �����Ѵ�.
+	
 		props=new Properties();
 		
 		FileInputStream fis=null;
 		try {
 			fis=new FileInputStream(realConfigFile);
-			props.load(fis);//load �޼��带 �̿���
+			props.load(fis);
 		}catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}catch (IOException e) {
@@ -68,60 +66,43 @@ public class DispatcherServlet extends HttpServlet {
 
 
 	private void requestPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//=>��ɾ� ó�� Ŭ������ ���Ͽ� �ش� Ŭ������ �޼��带 ȣ���Ѵ�
-		//- ��û �� �ش� URI���� ��ɾ�(contextPath�� ������ url)�� �����Ͽ� Properties �÷��ǿ��� Ű(��ɾ�)�� �ش��ϴ�
-		//value�� ��ɾ� ó�� Ŭ������ ���Ѵ�
-		//- ��ɾ� ó�� Ŭ������ �޼��带 ȣ���� �� ���������� ���� �޾Ƽ� ������ �Ѵ�
-		
-		//
+
 		response.setContentType("charset=utf-8");
-		//1.
+
 		request.setCharacterEncoding("utf-8");
 		
 		
 		
-		//1. ��û URI ������
-		String uri=request.getRequestURI();	//=> /mymvc/tips/book.do
+
+		String uri=request.getRequestURI();	
 		System.out.println("uri= "+uri);
 		
-		//2. uri���� Context path�� �����ؼ� ��ɾ� �κи� �����ϱ�
-		String contextPath=request.getContextPath();//=> /mymvc
+
+		String contextPath=request.getContextPath();
 		System.out.println("contextPath="+contextPath);
 		System.out.println("uri.indexOf(contextPath)="+uri.indexOf(contextPath));
 		
-		//��ɾ ����
+	
 		String key="";
 		if(uri.indexOf(contextPath)==0) {
-			key=uri.substring(contextPath.length());	//=> /tips/book.do
+			key=uri.substring(contextPath.length());
 		}
-		System.out.println("key="+key);//��ɾ�
+		System.out.println("key="+key);
 		
-		//��ɾ �ش��ϴ� ��ɾ� ó�� Ŭ���� ������
-		//getProperty�޼��带 ����ص��� key�� ���� value�� ���� ���ϴ� �޼���
-		String commandStr=props.getProperty(key);	//���ڿ� 
-		//=> com.tips.controller.BookController2
+		String commandStr=props.getProperty(key);	
+		
 		System.out.println("��ɾ� ó�� Ŭ����:"+commandStr+"\n");
 		
 		try {
-			//���ڿ��� Ŭ������
 			Class commandClass=Class.forName(commandStr);//forName()�� static �޼�����
-			//���ڿ� commandStr�� Ŭ������ �������ִ� forName() �޼���
 			
-			//Ŭ������ �ν��Ͻ���
 			Controller controller=(Controller)commandClass.newInstance();
-			//��ü�� �����ϱ� ���ؼ� newInstance() �޼��带 ����ϰ� ��ȯŸ���� object �̱� ������ �ٿ� ĳ������ �ؾ��ϴµ�
-			//������ controller�� ���������� �޾ƿ� �� �ִ� �������̽���  Controller�̱� ������ Controller�������̽��� ����
-			
-			//�޼��� ȣ��
 			String resultPage=controller.requestProcess(request, response);
 			System.out.println("resultPage= "+resultPage+"\n\n");
 			
-			//forward/redirect ���ο� ���� ó��
 			if(controller.isRedirect()) {
 				response.sendRedirect(contextPath+resultPage);
-				//=> /mymvc + /tips/book.do => /mymvc/tips/book.do
 			}else {
-				//�ش� �� �������� ������
 				RequestDispatcher dispatcher=request.getRequestDispatcher(resultPage);
 				dispatcher.forward(request, response);
 			}
@@ -138,3 +119,4 @@ public class DispatcherServlet extends HttpServlet {
 	
 	
 }
+//서블릿 주석삭제완료
