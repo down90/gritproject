854225fe6ec7fe11fe70a6b425c2769d3914369b
in440 <in440@DESKTOP-SRRN2RS>
2019-01-19 19:57:19 +0900

    강의리스트구현중

--- grit/src/com/grit/lecture/model/LectureService.java
+++ grit/src/com/grit/lecture/model/LectureService.java
@@ -1,6 +1,7 @@
 package com.grit.lecture.model;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class LectureService {
 	private LectureDAO lectureDao;
@@ -11,5 +12,17 @@ public class LectureService {
 	
 	public int insertClass(LectureVO vo) throws SQLException {
 		return lectureDao.insertClass(vo);
+	}
+	
+	public LectureVO selectReadCnt() throws SQLException{
+		return lectureDao.selectReadCnt();
+	}
+	
+	public List<LectureVO> selectRecommand() throws SQLException{
+		return lectureDao.selectRecommand();
+	}
+	
+	public List<LectureVO> selectRegdate() throws SQLException{
+		return lectureDao.selectRegdate();
 	}
 }
